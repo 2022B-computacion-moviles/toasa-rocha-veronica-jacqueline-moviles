896de362370e48c95e134b33f8cc69c1b8e08d0b
Veronica Toasa <veronica.toasa@epn.edu.ec>
2023-03-04 18:39:02 -0500

    Firebase 2

---
+++ 01-Android/app/src/main/java/com/example/movcompvjtr/JFirebaseFirestore.kt
@@ -0,0 +1,11 @@
+package com.example.movcompvjtr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JFirebasFirestore : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jfirebas_firestore)
+    }
+}
